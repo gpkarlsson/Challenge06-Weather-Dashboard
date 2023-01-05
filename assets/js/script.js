d289c1aa60ea74c1d577a71732f9960293570b7b
gordonk <gpkarlsson72@gmail.com>
2023-01-04 18:19:26 -0600

    worked on layout, some js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
-var APIKey = '02de89be83267d4702049938b828e151'
-var city = Madison,wi
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-fetch(queryURL);
+var baseApiUrl = 'https://api.openweathermap.org';
+// var openweatherApiKey = '02de89be83267d4702049938b828e151';
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=02de89be83267d4702049938b828e151
+var nameCity = document.getElementById('inputCity');
+//fetch('https://api.openweathermap.org')
+// var city; 
+// var cities = [];
+// var queryUrl = "api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
+// var geoCodeUrl = "api.openweathermap.org/geo/1.0/direct?q=" + cityname, + statecode, + countrycode + "&limit=" + limit + "&appid=" + apiKey
